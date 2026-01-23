9cd264943edc081d654d885df0bff684f29f61aa
Minh Nguyen <tcminh.ng20@gmail.com>
2026-01-23 15:32:48 -0700

    Implement frontBack function to swap first and last characters

--- Warmup-1/frontBack.java
+++ Warmup-1/frontBack.java
@@ -1,1 +1,9 @@
-
+public String frontBack(String str) {
+  if (str.length() <= 1){
+    return str; 
+  }
+  
+  String mid = str.substring(1, str.length() - 1);
+  
+  return str.charAt(str.length() - 1) + mid + str.charAt(0);
+}
