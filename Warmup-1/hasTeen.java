2c5d22e3dc63657dd6c88ce4010f14e90ccf488b
Minh Nguyen <tcminh.ng20@gmail.com>
2026-01-24 13:27:12 -0700

    Implement hasTeen method to check teen ages

---
+++ Warmup-1/hasTeen.java
@@ -0,0 +1,8 @@
+public boolean hasTeen(int a, int b, int c) {
+  if((13 <= a && a <= 19) ||
+      (13 <= b && b <= 19) ||
+      (13 <= c && c <= 19)){
+        return true;
+      }
+  return false;
+}
