6e65a89473478f6edcf149b813ba88db0e1c6564
Minh Nguyen <tcminh.ng20@gmail.com>
2026-01-24 17:03:15 -0700

    Add in3050 method to check range conditions

---
+++ Warmup-1/in3050.java
@@ -0,0 +1,8 @@
+public boolean in3050(int a, int b) {
+  if(((30 <= a && a <= 40) && (30 <= b && b <= 40)) ||
+      (40 <= a && a <= 50) && (40 <= b && b <= 50)){
+        return true;
+      }
+      
+  return false;
+}
