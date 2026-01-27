b4881c1066a52236bfcf963401fabc51737d133e
Minh Nguyen <tcminh.ng20@gmail.com>
2026-01-27 09:58:25 -0700

    Implement last2 method to count last two chars

---
+++ Warmup-2/last2.java
@@ -0,0 +1,16 @@
+public int last2(String str) {
+  if(str.length() < 2){
+    return 0;
+  }
+  
+  String lastTwo = str.substring(str.length()-2);
+  
+  int count = 0;
+  
+  for(int i=0; i<str.length()-2; i++){
+    if(str.substring(i,i+2).equals(lastTwo)){
+      count++;
+    }
+  }
+  return count;
+}
