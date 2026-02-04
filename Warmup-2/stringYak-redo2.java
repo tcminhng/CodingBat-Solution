eaccdb1ba99c1a45e029f9f54c932f7a765ebc4a
Minh Nguyen <tcminh.ng20@gmail.com>
2026-02-03 23:33:29 -0700

    Add stringYak method to filter 'yak' from input
    Implement stringYak method to remove 'yak' from string

---
+++ Warmup-2/stringYak-redo2.java
@@ -0,0 +1,14 @@
+public String stringYak(String str) {
+    String result = "";
+  
+  for (int i=0; i<str.length(); i++) {
+    // Look for i starting a "yak" -- advance i in that case
+    if (i+2<str.length() && str.charAt(i)=='y' && str.charAt(i+2)=='k') {
+      i =  i + 2;
+    } else { // Otherwise do the normal append
+      result = result + str.charAt(i);
+    }
+  }
+  
+  return result;
+}
