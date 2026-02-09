df8a5e1fa823059f9d8649bc72bc2924123e51be
Minh Nguyen <tcminh.ng20@gmail.com>
2026-02-08 22:29:15 -0700

    Add twoChar method to extract substring
    Implement twoChar method to return substring based on index.

---
+++ String-1/twoChar.java
@@ -0,0 +1,12 @@
+public String twoChar(String str, int index) {
+  String result;
+  
+  if(str.length()-2 < index || index < 1){
+    result = str.substring(0,2);  
+  }
+  else{
+    result = str.substring(index, index + 2);
+  }
+  
+  return result;
+}
