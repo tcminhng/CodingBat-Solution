91e5cbc0ef5ad75589b66f354b37c36a0ca8e40f
Minh Nguyen <tcminh.ng20@gmail.com>
2026-02-10 23:14:56 -0700

    Add frontAgain method to check string front and back

---
+++ String-1/frontAgain.java
@@ -0,0 +1,8 @@
+public boolean frontAgain(String str) {
+  if(str.length() >= 2 && str.substring(0,2).equals(str.substring(str.length()-2))){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
