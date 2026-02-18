22ad1ed9a86d29fece53da7f1b2ca7cd8b27aaf7
Minh Nguyen <tcminh.ng20@gmail.com>
2026-02-17 23:38:41 -0700

    Create double23.java

---
+++ Array-1/double23.java
@@ -0,0 +1,10 @@
+public boolean double23(int[] nums) {
+  if(nums.length < 2){
+    return false;
+  }
+  else if((nums[0] == 2 && nums[1] == 2) || (nums[0] == 3 && nums[1] == 3)){
+    return true;
+  }
+  
+  return false;
+}
